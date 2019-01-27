b0b8748bf8170c6719079d394413ec58e1ba3b84
朱驊 <>
2019-01-27 20:59:28 +0800

    update

---
+++ rotatedDigits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotatedDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        def isValidRotation(num):
+            return (not any(s in {'3', '4', '7'} for s in num)) and any(s in {'2', '5', '6', '9'} for s in num)
+
+        return sum([isValidRotation(str(i)) for i in range(1, N+1)])
